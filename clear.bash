d0f16744b8d67fb313bacbb20f4498e7e3df41da
Jakub Lenner <jakub.lenner@protonmail.com>
2023-03-02 18:00:55 +0100

    added script for deleting generated files

---
+++ clear.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# we are deleting filex explicitly so we delete only what we want to delete
+rm hmac_hey_plaintext_part_1.gif hmac_hey_plaintext_part_2.gif hmac_key hmac_key_part_1 hmac_key_part_2 key.png key.total.gif
