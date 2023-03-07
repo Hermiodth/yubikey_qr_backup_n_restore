43a0346d5fe37ab38ea6436fa1ebec1e55606625
Jakub Lenner <jakub.lenner@protonmail.com>
2023-03-07 18:27:46 +0100

    added some little tweaks and comments

--- generate_backup.bash
+++ generate_backup.bash
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+# 0. dependencies that we need to install first
 #sudo apt install yubikey-personalization qrencode
 
-dd status=none if=/dev/random bs=10 count=1 | xxd -p -c 20 > hmac_key_part_1
-dd status=none if=/dev/random bs=10 count=1 | xxd -p -c 20 > hmac_key_part_2
+# 1. we create two random byte arrays, 10 bytes each and save it to corresponding files
+dd status=none if=/dev/random bs=10 count=1 | xxd -p -c 10 > hmac_key_part_1
+dd status=none if=/dev/random bs=10 count=1 | xxd -p -c 10 > hmac_key_part_2
+
+# 2. then we concatenate those byte arrays into one key
 echo "$(cat hmac_key_part_1)$(cat hmac_key_part_2)" > hmac_key
+
+# 3. and save in the form of a QR code
 qrencode -o key.png -r hmac_key -s 18 -l H
+
+# 4. then we convert two parts of a key to the images so we can add them unther the QR code and print it together
 convert -pointsize 70 "label:$(cat hmac_key_part_1)" "hmac_hey_plaintext_part_1.gif"
 convert -pointsize 70 "label:$(cat hmac_key_part_2)" "hmac_hey_plaintext_part_2.gif"
+
+# 5. finally, we concatenate qr code with plaintext key and create one printable image
 convert -append key.png hmac_hey_plaintext_part_1.gif hmac_hey_plaintext_part_2.gif key.total.gif
--- restore_from_backup.bash
+++ restore_from_backup.bash
@@ -1,6 +1,24 @@
 #!/bin/bash
 
+# dependencies installation
 #sudo apt install zbar-tools
 
+# 1. oad the QR code and save the key to a variable
 YUBI_KEY=$(zbarcam -1 --raw)
+
+# 2. save key from te QR code to the yubikey
+#   -2 - upload to slot number 2
+#   -a$YUBI_KEY - provide key as AES
+#   -y - do not ask for permission, if zbarcam has done its work, copy key to the yubikey immediately
 ykpersonalize -2 -a$YUBI_KEY -ochal-resp -ochal-hmac -ohmac-lt64 -oserial-api-visible -oallow-update -y
+
+
+
+
+# Note: If you are unable to read the code from the camera (due to it being broken, taped, or for any other reason), you can use the following command:
+
+#		ykpersonalize -2 -a -ochal-resp -ochal-hmac -ohmac-lt64 -oserial-api-visible -oallow-update
+
+#	Please note that the switch '-a' does not have a specified key. You will be prompted to enter the key manually
+#	from the command line, which means you need to read it from the plaintext located under the QR code. Additionally,
+#	there is no switch '-y', so you will need to confirm manually.
